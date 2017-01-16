2b74450ceafb351dd48d8aa684dac315ff555a27
肖大宝 <xiaojin1992@126.com>
2017-01-16 22:46:17 +0800

    端口修改

--- file_server.js
+++ file_server.js
@@ -36,6 +36,6 @@ var server = http.createServer(function (request, response) {
     });
 });
 
-server.listen(8080);
+server.listen(8090);
 
-console.log('Server is running at http://127.0.0.1:8080/');
+console.log('Server is running at http://127.0.0.1:8090/');
