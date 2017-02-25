c39087fea2928ce76f5faa14f8a75eea5e6511fe
肖大宝 <xiaojin1992@126.com>
2017-02-25 21:36:38 +0800

    no message

--- file_server.js
+++ file_server.js
@@ -39,6 +39,6 @@ var server = http.createServer(function (request, response) {
     });
 });
 
-server.listen(8090);
+server.listen(8010);
 
-console.log('Server is running at http://127.0.0.1:8090/');
+console.log('Server is running at http://127.0.0.1:8010/');
